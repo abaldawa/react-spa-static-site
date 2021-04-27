104c93966e6f7606ac98e8551ad15f5dd1bd184d
Abhijit <abhi.baldawa@gmail.com>
2021-04-27 20:13:11 +0200

    handled 404 routing errors from s3 on react app

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserHistory} from 'history';
+
+const browserHistory = createBrowserHistory();
+
+
+/**
+ * Basically if URL is of format "https://url.com/#!/page1" then below
+ * regexp will extract "/page1" from the above URL
+ */
+const path = (/#!(\/.*)$/.exec(window.location.hash) || [])[1];
+
+
+if (path) {
+    /**
+     * If there was an existing path then replace the state of the
+     * browser history. This will not make any network call to the
+     * backend just will replace the history so our react SPA router
+     * can route to appropriate page
+     */
+    browserHistory.replace(path);
+}
 
 ReactDOM.render(
   <React.StrictMode>
